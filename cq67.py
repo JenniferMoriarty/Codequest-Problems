6590c200acb56f9244dd1585f14e4884e55085bd
Jen Moriarty <JMoriarty78@gmail.com>
2024-01-12 13:09:14 -0700

    Add files via upload

---
+++ cq67.py
@@ -0,0 +1,11 @@
+import sys #needed for readline and others
+vowels=0
+cases = int(sys.stdin.readline().rstrip()) #gets the number of lines
+
+for caseNum in range(cases):
+    line = sys.stdin.readline().rstrip() #walks through each line
+    for character in line: #walks through each letter in the line
+        if character=='a' or character=='e' or character=='i' or character=='o' or character=='u':
+            vowels+=1
+    print(vowels)
+    vowels = 0 #reset for next line
